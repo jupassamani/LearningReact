a005d007ae016971917ee20c4c1a5747d36dade0
Julia Passamani <juhpassamani@hotmail.com>
2018-05-02 10:55:22 -0300

    blinking

--- App.js
+++ App.js
@@ -4,24 +4,4 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, Text, View, Image} from 'react-native';
 
-class Salutation extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name} ! </Text>
-    );
-  }
-}
-
-class GreatSalutation extends Component {
-  render() {
-    return (
-      <View style={{alignItems:'center'}}>
-        <Salutation name='Salutation1' />
-        <Salutation name='Salutation2' />
-      </View>
-    );
-  }
-}
