2f9fb92c135431a78fdb5c901fd47083b8088187
Julia Passamani <juhpassamani@hotmail.com>
2018-05-02 11:42:33 -0300

    ListView Example added - right file

--- App.js
+++ App.js
@@ -7,29 +7,3 @@
 
 import React, {Component} from 'react';
 import { AppRegistry, View, Text, ListView } from 'react-native';
-
-class ListViewExample extends Component {
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
-
-    this.state = { 
-      dataSource: ds.cloneWithRows([
-        'Julia', 'Bruna', 'Ligia', 'Jorge', 'Rosi'
-      ])
-
-     };
-  }
-  render() {
-    return (
-      <View style={{paddingTop: 22}}>
-        <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
-      </View>
-    );
-  }
-}
-
-export default ListViewExample;
