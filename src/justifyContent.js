b13443f67fd9e703edd1c9c4b31c8b83ec5da6b3
Julia Passamani <juhpassamani@hotmail.com>
2018-05-02 11:26:54 -0300

    Justifying content example

--- src/justifyContent.js
+++ src/justifyContent.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import { AppRegistry, View } from 'react-native';
 
-class FlexDirectionExample extends Component {
+class JustifyingContent extends Component {
   render() {
     return (
-      //Set the flexDirection to row
-      <View style={{flex:1, flexDirection:'row'}}>
+      // set the justifyContent to center
+      // set the flexDirection to column
+      <View style={{
+        flex:1, 
+        flexDirection:'column',
+        justifyContent:'space-between'
+      }}>
         <View style={{width: 50, height: 50, backgroundColor:'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor:'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor:'steelblue'}} />
@@ -14,4 +19,4 @@ class FlexDirectionExample extends Component {
   }
 }
 
-export default FlexDirectionExample;
+export default JustifyingContent;
