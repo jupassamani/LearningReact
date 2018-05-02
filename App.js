6bd78e58b8afc3839e47dd6615d4e1ea7d65ee58
Julia Passamani <juhpassamani@hotmail.com>
2018-05-02 11:10:14 -0300

    Fixed Dimensions Example

--- App.js
+++ App.js
@@ -8,27 +8,3 @@
 import React, {Component} from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class NumerousStyles extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={styles.red}> a red </Text>
-        <Text style={styles.bigblue}> a bigblue </Text>
-        <Text style={[styles.bigblue, styles.red]}> a bigblue and red </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  bigblue:{
-    color:'blue',
-    fontWeight:'bold',
-    fontSize: 30
-  },
-  red: {
-    color:'red'
-  }
-});
-
-export default NumerousStyles;
